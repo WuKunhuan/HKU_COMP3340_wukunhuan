b559191472862b15fe8cf660027eece1d8a81e48
Kunhuan Wu <u3577163@connect.hku.hk>
2022-11-06 12:55:10 +0800

    [Create] GPU Farm Setup

--- HKU CS GPU Farm/setup_gpu_farm.py
+++ HKU CS GPU Farm/setup_gpu_farm.py
@@ -1,1 +1,50 @@
 
+import os
+
+account = "uxxxxxxx"
+virtual_environment = "COMP3340"
+setting = open("foo.txt", "r").readlines()
+for line in setting:
+    if (len(line) > 12):
+        if (line[0:11] == "account = \""):
+            account = line[11:len(line)-1:1]
+            continue
+    if (len(line) > 24):
+        if (line[0:23] == "virtual_environment = \""):
+            virtual_environment = line[23:len(line)-1:1]
+            continue
+
+
+input("Login to your CS GPU Farm")
+os.system("clear")
+os.system(f"ssh -X {account}@gpu2gate1.cs.hku.hk")
+os.system("clear")
+
+input("Install Anaconda")
+os.system("clear")
+os.system("wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh")
+os.system("bash Anaconda3-2020.11-Linux-x86_64.sh")
+os.system("export PATH=/userhome/cs2/{account}/anaconda3/bin:$PATH")
+os.system("clear")
+
+input("Install pip")
+os.system("clear")
+os.system("conda install pip")
+os.system("clear")
+
+input("Install JupyterLab")
+os.system("clear")
+os.system("conda install jupyterlab")
+os.system("clear")
+
+input("Create a virtual environment")
+os.system("clear")
+os.system(f"conda create -n {virtual_environment} python=3.7 -y")
+os.system("clear")
+
+input("Install Pytorch, torchvision, cudatoolkit")
+os.system("clear")
+os.system("conda install pytorch==1.7.1 torchvision==0.8.2 cudatoolkit=10.1 -c pytorch")
+os.system("conda install torchvision")
+os.system("clear")
+
